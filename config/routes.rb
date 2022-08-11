c35a171ca658a0dd0ea16e2767bebcb0f3bf2669
maria <mjperez5122@gmail.com>
2022-08-11 01:35:01 -0400

    se agrego scaffold Genero y Producto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
    
+  resources :productos
+  resources :generos
   root 'home#index'
 end
---
+++ db/migrate/20220811052410_create_productos.rb
@@ -0,0 +1,12 @@
+class CreateProductos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :productos do |t|
+      t.string :titulo
+      t.integer :total_stock
+      t.string :descripcion
+      
+
+      t.timestamps
+    end
+  end
+end
