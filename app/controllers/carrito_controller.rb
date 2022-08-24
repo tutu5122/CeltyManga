8c0fefe09900496a51c60a38bf80556c4e642d0c
maria <mjperez5122@gmail.com>
2022-08-23 20:47:48 -0400

    se agrego user al panel admin

--- app/controllers/carrito_controller.rb
+++ app/controllers/carrito_controller.rb
@@ -1,7 +1,9 @@
 class CarritoController < ApplicationController
   before_action :authenticate_user!
+
   def show
     @orden_compras = OrdenCompra.all
     @detalle_compras = DetalleCompra.all
+
   end
 end
